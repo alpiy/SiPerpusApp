cdd5194876f277a22b73e34196f42fbd77b2e54d
alpiy <alfiahmad327@gmail.com>
2024-06-21 02:57:45 +0700

    fetch : modified list_member_activity

--- app/src/main/java/com/example/siperpus/activity/HomepageActivity.kt
+++ app/src/main/java/com/example/siperpus/activity/HomepageActivity.kt
@@ -8,6 +8,7 @@ import android.view.View.OnClickListener
 import android.widget.Button
 import com.example.siperpus.R
 import com.example.siperpus.buku.ListBukuActivity
+import com.example.siperpus.config.SharedPrafManager
 import com.example.siperpus.databinding.ActivityHomepageBinding
 import com.example.siperpus.member.ListMemberActivity
 
@@ -23,26 +24,32 @@ class HomepageActivity : AppCompatActivity(), OnClickListener {
         binding.returnBuku.setOnClickListener(this)
         binding.rentBuku.setOnClickListener(this)
         binding.manageMember.setOnClickListener(this)
+
     }
 
     override fun onClick(hm: View?) {
-        when(hm?.getId()){
+        when (hm?.getId()) {
             R.id.manage_buku -> {
                 val intentBook = Intent(this@HomepageActivity, ListBukuActivity::class.java)
                 startActivity(intentBook)
             }
+
             R.id.return_buku -> {
                 val intentReturn = Intent(this@HomepageActivity, ListBukuActivity::class.java)
                 startActivity(intentReturn)
             }
+
             R.id.rent_buku -> {
                 val intentRent = Intent(this@HomepageActivity, ListBukuActivity::class.java)
                 startActivity(intentRent)
             }
+
             R.id.manage_member -> {
                 val intentMember = Intent(this@HomepageActivity, ListMemberActivity::class.java)
                 startActivity(intentMember)
             }
+
         }
+
     }
 }
--- app/src/main/java/com/example/siperpus/member/ListMemberActivity.kt
+++ app/src/main/java/com/example/siperpus/member/ListMemberActivity.kt
@@ -1,11 +1,29 @@
 package com.example.siperpus.member
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
 import com.example.siperpus.R
+import com.example.siperpus.buku.UpdateAddBukuActivity
+import com.example.siperpus.databinding.ActivityListBukuBinding
+import com.example.siperpus.databinding.ActivityListMemberBinding
 
-class ListMemberActivity : AppCompatActivity(){
+class ListMemberActivity : AppCompatActivity(), OnClickListener{
+    private lateinit var binding : ActivityListMemberBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_list_member)
+        binding = ActivityListMemberBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnAddM.setOnClickListener(this)
+    }
+    override fun onClick(v: View?) {
+        when(v?.getId()){
+            R.id.btnAdd_m -> {
+                val intentMem = Intent(this@ListMemberActivity, UpdateAddMemberActivity::class.java)
+                startActivity(intentMem)
+            }
+        }
     }
 }
