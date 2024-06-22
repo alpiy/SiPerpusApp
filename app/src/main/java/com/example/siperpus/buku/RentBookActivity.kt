851500adc218e0b0bafab609eb3c44602807bf7e
alpiy <alfiahmad327@gmail.com>
2024-06-22 23:51:15 +0700

    fetch : modified update rent and return book

--- app/src/main/java/com/example/siperpus/buku/RentBookActivity.kt
+++ app/src/main/java/com/example/siperpus/buku/RentBookActivity.kt
@@ -1,10 +1,12 @@
 package com.example.siperpus.buku
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.view.View
 import android.view.View.OnClickListener
 import androidx.appcompat.app.AppCompatActivity
+import com.example.siperpus.R
 import com.example.siperpus.databinding.ActivityListBukuBinding
 import com.example.siperpus.databinding.ActivityRentBookBinding
 
@@ -21,6 +23,11 @@ class RentBookActivity: AppCompatActivity(), OnClickListener {
     }
 
     override fun onClick(v: View?) {
-
+        when(v?.getId()){
+            R.id.btnAdd_rbn -> {
+                val intentUpdate = Intent(this@RentBookActivity, UpdateAddRentBook::class.java)
+                startActivity(intentUpdate)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/siperpus/buku/ReturnBookActivity.kt
+++ app/src/main/java/com/example/siperpus/buku/ReturnBookActivity.kt
@@ -1,9 +1,11 @@
 package com.example.siperpus.buku
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.view.View.OnClickListener
 import androidx.appcompat.app.AppCompatActivity
+import com.example.siperpus.R
 import com.example.siperpus.databinding.ActivityRentBookBinding
 import com.example.siperpus.databinding.ActivityReturnBookBinding
 
@@ -15,10 +17,16 @@ class ReturnBookActivity : AppCompatActivity(), OnClickListener {
         binding = ActivityReturnBookBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btnAddRb.setOnClickListener(this)
 
     }
 
     override fun onClick(v: View?) {
-
+        when(v?.getId()){
+            R.id.btnAdd_rb -> {
+                val intentUpdate = Intent(this@ReturnBookActivity, UpdateAddReturnBook::class.java)
+                startActivity(intentUpdate)
+            }
+        }
     }
 }
