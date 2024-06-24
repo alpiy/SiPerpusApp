65f1c5da33d38f62074bafed9ebe34907b505a55
Syaiful124 <syaifulmaarif124@gmail.com>
2024-06-25 04:39:26 +0700

    fetch: get data member

--- app/src/main/java/com/example/siperpus/buku/ListBukuActivity.kt
+++ app/src/main/java/com/example/siperpus/buku/ListBukuActivity.kt
@@ -3,32 +3,18 @@ package com.example.siperpus.buku
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.View.OnClickListener
-import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.siperpus.R
 import com.example.siperpus.adapter.StoriesAdapter
-import com.example.siperpus.config.ApiClient
-import com.example.siperpus.config.createRetrofitService
 import com.example.siperpus.databinding.ActivityListBukuBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-
 @AndroidEntryPoint
 class ListBukuActivity : AppCompatActivity(), OnClickListener {
     private lateinit var binding: ActivityListBukuBinding
-
-    private val apiService by lazy { createRetrofitService() }
     private val viewModel by viewModels<MainViewModel> {
         ViewModelFactory.getInstance()
     }
@@ -47,12 +33,7 @@ class ListBukuActivity : AppCompatActivity(), OnClickListener {
                 setStoriesData(listBook)
             }
         }
-
-
-
         binding.btnTambahB.setOnClickListener(this)
-
-
     }
     private fun setupRecyclerView() {
         val layoutManager = LinearLayoutManager(this)
